0588ae331230a8d130bea0616190d9d6743eba26
seochan99 <gmlcks0513@naver.com>
2021-09-24 14:03:37 +0900

    templates2

--- floo/main/views.py
+++ floo/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main(request):
+    return render(request,'main.html')
+
+def showhome(request):
+    return render(request, 'home.html')
