5b2a616bea2af982f5e8b628f1e7026bdbce2a3e
soosbk <soobin851@kookmin.ac.kr>
2021-09-24 14:16:39 +0900

    model 변경

--- floo/mbti/models.py
+++ floo/mbti/models.py
@@ -1,6 +1,2 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-class UserModel(AbstractUser):
-    count_num = models.IntegerField(default=0, blank=True, null=True)
-    
