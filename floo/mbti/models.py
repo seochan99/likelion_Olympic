6e9f7380847a08e4e64a6761158069d42f157c7c
soosbk <soobin851@kookmin.ac.kr>
2021-09-24 11:43:04 +0900

    app 만들기

--- floo/mbti/models.py
+++ floo/mbti/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class UserModel(AbstractUser):
+    count_num = models.IntegerField(default=0, blank=True, null=True)
