932b50cd575a27d519c7deabb6dcee6e1621eacd
soosbk <soobin851@kookmin.ac.kr>
2021-09-25 09:55:21 +0900

    model 충돌 수정

--- floo/account/models.py
+++ floo/account/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 RESULT_CHOICES={
     ('yolo','YOLO족'),
-    ('fire','FIRE족')
+    ('fire','FIRE족'),
+    ('null','미선택')
 }
 
 # Create your models here.
@@ -12,6 +13,5 @@ class CustomUser(AbstractUser):
     age = models.CharField(max_length=10) 
     nickname = models.CharField(max_length=15)
     profile= models.ImageField(blank=True, null=True)
-    result=models.CharField(max_length=5, choices=RESULT_CHOICES,blank=True,null=True)
-    count_num = models.IntegerField(default=0, blank=True, null=True)
+    result=models.CharField(max_length=5, choices=RESULT_CHOICES,default='null')
     
