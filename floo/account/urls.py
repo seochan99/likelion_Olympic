30b8e0e6e411950f63b809233cfc030430d97e97
seochan99 <gmlcks0513@naver.com>
2021-09-25 11:42:46 +0900

    front/2

--- floo/account/urls.py
+++ floo/account/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.urls.resolvers import URLPattern
 from .views import *
 
+app_name = 'account'
+
 urlpatterns=[
     path('register/',register_view,name="register"),
     path('login/',login_view,name="login"),
