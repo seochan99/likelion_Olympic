38437005a69c7cf032e1c80c373be850b085aa89
WATERB(soosbk) <66404477+soosbk@users.noreply.github.com>
2021-09-25 13:32:31 +0900

    로그아웃까지 ok

--- floo/account/urls.py
+++ floo/account/urls.py
@@ -7,4 +7,6 @@ app_name = 'account'
 urlpatterns=[
     path('register/', register_view, name="register"),
     path('login/',login_view,name="login"),
+    path('logout/', logout_view, name="logout"),
+
 ]
